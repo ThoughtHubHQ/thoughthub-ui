d313d7a532b33ba7c859c8e5383ff8d7843e042f
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2026-01-27 01:41:51 +0600

    Adjust min height of hero section on homepage
    Changed the min-h value of the main logo section from 96vh to 88vh to improve layout or visual balance on the homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       {/* Demo Screen */}
       <section className="bg-[#e7eacd] text-center min-h-screen">
         <Link href="https://thoughthubhq.com">
-          <div className=" flex-col flex justify-center items-center min-h-[96vh] bg-[url('/logo/th-logo.png')] bg-no-repeat bg-center bg-contain" />
+          <div className=" flex-col flex justify-center items-center min-h-[88vh] bg-[url('/logo/th-logo.png')] bg-no-repeat bg-center bg-contain" />
         </Link>
         <span className="font-semibold flex justify-center items-center text-center gap-1">
           &copy; {new Date().getFullYear()} ThoughtHub{" "}
