34faa9bff7abe87188236ab2d24a378b74e0a108
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2026-05-13 10:36:28 +0600

    updated logo on manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,19 +4,20 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "ThoughtHub - Where Thoughts Take Shape",
     short_name: "ThoughtHub",
-    description: "Innovative solutions for Web & Mobile apps Development, UI/UX & Creative Design, Notion Workspace Setup & IT Consultancy.",
+    description:
+      "Innovative solutions for Web & Mobile apps Development, UI/UX & Creative Design, Notion Workspace Setup & IT Consultancy.",
     start_url: "/",
     display: "standalone",
     background_color: "#e7e9cd",
     theme_color: "#e7e9cd",
     icons: [
       {
-        src: "/logo/th-logo-192.png",
+        src: "/logo/th-banner-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/logo/th-logo-512.png",
+        src: "/logo/th-banner-512.png",
         sizes: "512x512",
         type: "image/png",
       },
--- components/custom/Footer.tsx
+++ components/custom/Footer.tsx
@@ -56,8 +56,7 @@ export const socialMediaLinks = [
 export default function Footer() {
   return (
     <div className="py-5 bg-transparent border-t border-black/20 dark:border-[#e7eacd]/20 text-sm text-black/70 dark:text-[#e7eacd]/70">
-      <div className="w-full flex flex-col md:flex-row items-center justify-between text-center gap-4 max-w-360 mx-auto">
-        {" "}
+      <div className="w-full flex flex-col md:flex-row items-center justify-between text-center gap-4 max-w-360 mx-auto px-6 lg:px-8">
         <div>
           &copy; {new Date().getFullYear()}{" "}
           <span className={`font-bold text-black dark:text-[#e7eacd] ${roxborough.className}`}>
